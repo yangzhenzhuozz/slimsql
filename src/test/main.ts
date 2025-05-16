7d33aa51224ed2f3176fbb4ff51c40597761b5b6
john <yangzhenzhuozz@126.com>
2025-05-16 21:44:49 +0800

    重命名项目为 slimsql，更新版本号至 1.0.0，修改描述信息，调整 README 和 tsconfig 配置

--- src/test/main.ts
+++ src/test/main.ts
@@ -25,33 +25,11 @@ session.registTableView(arr3, 't3');
 
 console.time('Execution Time');
 let ret = session.sql(`
-select distinct * from t3
+select
+  rank() over(partition by t1.id order by score) as rn , *
+from
+  t1 left join t2 on t1.id=t2.id
+  left join t3 on t2.id=t3.id
 `);
 console.table(ret.data);
 console.timeEnd('Execution Time');
-
-/*
-1、测试alias                                    OK
-2、测试order by                                 OK
-    1、order by windowFunction                 OK
-3、limit a,b    (a+b>rowSize)                   OK
-4、测试group by以及group by之后的开窗           OK
-    1、group by字段能否select                   OK
-    2、group by表达式能否select                 OK
-    3、没有使用group by就聚合                   OK 
-5、实现windowFunction,并测试在order by中的效果   OK
-    1、测试聚合函数全选                          OK
-    2、测试聚合函数部分                          OK
-    2、测试order by                             OK
-6、连接                                        OK
-    1、两表连接                                 OK
-    2、三表连接                                 OK
-    3、连接时左表或者右表没有任何行              OK
-7、测试where                                    OK
-8、测试                                         OK
-    select
-      A.id //这里应该报错
-    from
-      (select * from t1) as A
-
- */
